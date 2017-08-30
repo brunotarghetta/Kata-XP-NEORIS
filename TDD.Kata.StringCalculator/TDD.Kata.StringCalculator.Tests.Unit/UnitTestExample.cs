d5ea6d40948450d7e3170f0a6afc89e401f10c98
Bruno Targhetta <brunotarghetta@gmail.com>
2017-08-30 10:13:07 -0300

    Primer test

--- TDD.Kata.StringCalculator/TDD.Kata.StringCalculator.Tests.Unit/UnitTestExample.cs
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator.Tests.Unit/UnitTestExample.cs
@@ -4,24 +4,37 @@ namespace TDD.Kata.StringCalculator.Tests.Unit
 {
     public class UnitTestExample
     {
-        //private Target Target;
+        private StringCalculator Target;
 
         public UnitTestExample()
         {
-            //this.Target = new StringCalculator();
+            this.Target = new StringCalculator();
         }
 
         [Fact]
         public void Example()
         {
             //Arrange
-            //Your set up!.
+            //Your set up!. 
 
             //Act
             //Your principal action to test.
 
             //Assert
             //Your expected value.
+        }
+
+
+        [Fact]
+        public void debe_devolver_cero_si_paso_vacio()
+        {
+            //Arrange
+            
+            //Act
+            var result = this.Target.Add();
+
+            //Assert
+            Assert.Equal(result, 0);
         }
     }
 }
---
+++ TDD.Kata.StringCalculator/TDD.Kata.StringCalculator/StringCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Kata.StringCalculator
+{
+    public class StringCalculator
+    {
+        public int Add()
+        {
+            return 0;
+        }
+    }
+}
